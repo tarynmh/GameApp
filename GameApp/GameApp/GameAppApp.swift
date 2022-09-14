4d011d16024eacef07f468f6fb775ad41be53b8a
Taryn Hovenkamp <taho3676@colorado.edu>
2022-09-14 10:58:14 -0600

    Initial Commit
    Initial Setup of Application

---
+++ GameApp/GameApp/GameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppApp.swift
+//  GameApp
+//
+//  Created by CUBS Customer on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
